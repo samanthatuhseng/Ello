d29b5c5dd256fc3de0bc512d98c13468e61e6dbc
DonaldKLee <Donald3010@outlook.com>
2022-09-17 02:35:14 -0400

    Component test

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Welcome } from './assets/components/testcomponent'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Donald Test!</Text>
+      <Welcome/>
+
+
       <StatusBar style="auto" />
     </View>
   );
